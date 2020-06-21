9338057c912fa8989f6856e641206102f8a515dc
TadjievCodes <muhammad2008.08@mail.ru>
2020-06-20 22:15:22 -0400

    Rndom color

---
+++ js/script.js
@@ -0,0 +1,111 @@
+let html = '';
+let red;
+let green;
+let blue;
+let randomRGB;
+
+
+const randomColour = () => Math.floor(Math.random() * 256);
+
+// Not using retrun keyword as arrow function one line doesn't need it
+
+
+function randomRGB () {
+
+const color = ;
+
+}
+
+
+for(let i = 1; i <= 10; i++){
+
+red = randomColour();
+green = randomColour();
+blue = randomColour();
+randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+html += `<div style="background-color: ${randomRGB}">${i}</div>`;
+  
+}
+
+
+
+
+
+
+ 
+
+
+document.querySelector('main').innerHTML = html;
+
+
+
+/*
+
+
+
+red = Math.floor(Math.random() * 256);
+green = Math.floor(Math.random() * 256);
+blue = Math.floor(Math.random() * 256);
+randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+html += `<div style="background-color: ${randomRGB}">1</div>`;
+
+red = Math.floor(Math.random() * 256);
+green = Math.floor(Math.random() * 256);
+blue = Math.floor(Math.random() * 256);
+randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+html += `<div style="background-color: ${randomRGB}">2</div>`;
+
+red = Math.floor(Math.random() * 256);
+green = Math.floor(Math.random() * 256);
+blue = Math.floor(Math.random() * 256);
+randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+html += `<div style="background-color: ${randomRGB}">3</div>`;
+
+red = Math.floor(Math.random() * 256);
+green = Math.floor(Math.random() * 256);
+blue = Math.floor(Math.random() * 256);
+randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+html += `<div style="background-color: ${randomRGB}">4</div>`;
+
+red = Math.floor(Math.random() * 256);
+green = Math.floor(Math.random() * 256);
+blue = Math.floor(Math.random() * 256);
+randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+html += `<div style="background-color: ${randomRGB}">5</div>`;
+
+red = Math.floor(Math.random() * 256);
+green = Math.floor(Math.random() * 256);
+blue = Math.floor(Math.random() * 256);
+randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+html += `<div style="background-color: ${randomRGB}">6</div>`;
+
+red = Math.floor(Math.random() * 256);
+green = Math.floor(Math.random() * 256);
+blue = Math.floor(Math.random() * 256);
+randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+html += `<div style="background-color: ${randomRGB}">7</div>`;
+
+red = Math.floor(Math.random() * 256);
+green = Math.floor(Math.random() * 256);
+blue = Math.floor(Math.random() * 256);
+randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+html += `<div style="background-color: ${randomRGB}">8</div>`;
+
+red = Math.floor(Math.random() * 256);
+green = Math.floor(Math.random() * 256);
+blue = Math.floor(Math.random() * 256);
+randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+html += `<div style="background-color: ${randomRGB}">9</div>`;
+
+red = Math.floor(Math.random() * 256);
+green = Math.floor(Math.random() * 256);
+blue = Math.floor(Math.random() * 256);
+randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+html += `<div style="background-color: ${randomRGB}">10</div>`;
+*/
+
+
+
+
+
+
